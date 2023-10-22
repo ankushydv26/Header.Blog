cbb8666f6093289bdb714c5a876d9005a3bcb537
Ankush Yadav <Noreply>
2023-10-22 23:29:06 +0530

    added header blog

---
+++ src/app/component/Footer.js
@@ -0,0 +1,10 @@
+const Footer =() => {
+    return(
+        <footer className="container">
+            <hr/>    
+            <p>&copy; 2021 - My Website</p>
+        </footer>        
+    )
+}
+
+export default Footer;
---
+++ src/app/component/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+
+    return(
+        <header className="container pt-80 pb-80">
+            <div className="h1">Header Blog</div>
+        </header>
+    )
+}
+
+export default Header;
