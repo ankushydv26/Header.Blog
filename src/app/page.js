871d1bfec8f1925f5abc80911dc44c46705ca633
Ankush Yadav <Noreply>
2023-10-23 20:14:57 +0530

    fix the page layout

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,21 @@
-import Card from "./component/Card"
+import Card from "./component/Card";
 export default function Home() {
   return (
     <div className="container">
-        <Card/>
+      <Card />
+      <div className="row_wrap">
+        <div className="row">
+          <div className="col col-4">
+            <Card />
+          </div>
+          <div className="col col-4">
+            <Card />
+          </div>
+          <div className="col col-4">
+            <Card />
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
