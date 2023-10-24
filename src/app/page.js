5a7147be7a4f0012202b97f367e9a40daa70bd1b
Ankush Yadav <Noreply>
2023-10-24 12:56:34 +0530

    change responsive way to better layout

--- src/app/page.js
+++ src/app/page.js
@@ -3,18 +3,18 @@ export default function Home() {
   return (
     <div className="container">
       <Card />
-      <div className="row_wrap">
+      {/* <div className="row_wrap"> */}
         <div className="row">
-          <div className="col col-4">
+          <div className="col col-4 min-mw-100">
             <Card />
           </div>
-          <div className="col col-4">
+          <div className="col col-4 min-mw-100">
             <Card />
           </div>
-          <div className="col col-4">
+          <div className="col col-4 min-mw-100">
             <Card />
           </div>
-        </div>
+        {/* </div> */}
       </div>
     </div>
   );
