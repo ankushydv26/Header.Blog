133227958c0bf2e6acfdfaf62afc46f521ca8628
Ankush Yadav <Noreply>
2023-10-24 13:53:13 +0530

    added helper for category the color based on their label

---
+++ src/app/helper/getCategory-color.js
@@ -0,0 +1,10 @@
+const categoryColorMap = new Map();
+
+categoryColorMap.set("Product Review", "orange");
+categoryColorMap.set("Opinions", "green");
+categoryColorMap.set("Travel Guides", "purple");
+
+const getCategoryColor = (category) => {
+  return categoryColorMap.get(category);
+};
+export default getCategoryColor;
